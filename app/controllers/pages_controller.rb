cc8f9e02585c38c5684d0f96134043e2ba3ff474
Raquel Minasi <raquelminasi@gmail.com>
2019-08-20 19:20:52 +0100

    search form partially done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    @bike = Bike.all
+    if @bike.nil?
+      @bike = Bike.where(name: "")
+    else
+      @bike = Bike.where(name: params)
+    end
+  end
 end
