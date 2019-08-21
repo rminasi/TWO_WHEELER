2ad7b69e36babd69a7eb8031ca08660831dad2fc
Susan Renaud <serenaud89@gmail.com>
2019-08-21 06:50:08 -0500

    made it all work G

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @bike = Bike.find(params[:bike_id])
+    @booking = Booking.new(booking_params)
+    @booking.renter = current_user
+    @booking.bike = @bike
+    if @booking.save
+      redirect_to bike_booking_path(@bike, @booking)
+    else
+      render bike_path(@bike)
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+
+  def booking_params
+    params.require(:booking).permit(:rental_date, :return_date, :bike_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_160219) do
+ActiveRecord::Schema.define(version: 2019_08_21_111154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2019_08_20_160219) do
     t.string "image"
     t.string "name"
     t.text "description"
+    t.string "location"
+    t.date "rent_date"
+    t.date "return_date"
+    t.float "price_per_day"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
@@ -32,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_08_20_160219) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "rental_date"
+    t.date "return_date"
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
