156bfc4fc0beff2984182e0d8618e9ee59e207ce
Susan Renaud <serenaud89@gmail.com>
2019-08-22 10:19:25 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,8 @@ puts "Creating 5 users"
   puts "Creating 3 bike for #{user.email}"
   3.times do |i|
     bike_url = "https://images.unsplash.com/photo-1507035895480-2b3156c31fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-    bike = Bike.new(name: "Touring Bike", description: "great bike", owner: user, location: ['Hackney, UK', 'Paris, France', 'Berlin, Germany'].sample)
+    bike = Bike.new(name: ["Touring Bike", "Great Bike", "Mountain Bike", "Cross Bike", "Road Bike"].sample, description: ["great bike", "This bike can really get you around", "I love this bike. Treat it nicely.", "Super bike up for rental while I'm out of town.", "Just changed wheels and bled brakes. Works great."
+    ].sample, owner: user, location: ['Hackney, UK', 'Paris, France', 'Berlin, Germany'].sample)
     bike.image = bike_url
     bike.save!
   end
